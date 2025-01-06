96c90ba1a818b71e40c83a611e611747940e39c8
Wend4r <47463683+Wend4r@users.noreply.github.com>
2025-01-06 05:30:05 +0300

    Chat command system: update header marco

--- include/sample/chat_command_system.hpp
+++ include/sample/chat_command_system.hpp
@@ -19,8 +19,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#ifndef _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_HPP_
-#	define _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_HPP_
+#ifndef _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_SYSTEM_HPP_
+#	define _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_SYSTEM_HPP_
 
 #	pragma once
 
@@ -104,4 +104,4 @@ namespace Sample
 	}; // MenuSystem::ChatCommandSystem
 }; // Sample
 
-#endif // _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_HPP_
+#endif // _INCLUDE_METAMOD_SOURCE_SAMPLE_CHAT_COMMAND_SYSTEM_HPP_
